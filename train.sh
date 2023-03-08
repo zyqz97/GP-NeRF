f90ce8c93118ef9398961dd4ca602cc1914cd040
Yuqi ZHANG <48278265+zyqz97@users.noreply.github.com>
2023-03-08 11:39:48 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@ exp_name='logs/test'
 
 dataset1='Mill19'  #  "Mill19"  "Quad6k"   "UrbanScene3D"
 dataset2='building' #  "building"  "rubble"  "quad"  "residence"  "sci-art"  "campus"
-python gp_nerf/train.py     --config_file  configs/$dataset2.yaml   --dataset_path  /data/yuqi/Datasets/MegaNeRF/$dataset1/$dataset2/$dataset2-pixsfm    --chunk_paths   /data/yuqi/Datasets/MegaNeRF/$dataset1/${dataset2}_chunk-1      --exp_name  $exp_name #   --cluster_mask_path   /data/yuqi/Datasets/MegaNeRF/$dataset1/$dataset2/$dataset2-pixsfm-grid-1-1/0
+python gp_nerf/train.py     --config_file  configs/$dataset2.yaml   --dataset_path  /data/yuqi/Datasets/MegaNeRF/$dataset1/$dataset2/$dataset2-pixsfm    --chunk_paths   /data/yuqi/Datasets/MegaNeRF/$dataset1/${dataset2}_chunk-1      --exp_name  $exp_name
 
 
 
